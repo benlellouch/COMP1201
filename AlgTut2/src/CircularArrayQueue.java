897ead89cb8877b385f331e5ad33972bfd334c6c
benlellouch <blellouch@gmail.com>
2019-03-15 10:38:35 +0000

    Just added some comments and removed mutator().

--- AlgTut2/src/CircularArrayQueue.java
+++ AlgTut2/src/CircularArrayQueue.java
@@ -15,23 +15,21 @@ public class CircularArrayQueue implements MyQueue {
         this.circularArrayQueue = new Integer[10];
     }
     @Override
-    public void enqueue(int in) {
+    public void enqueue(int input) {
 
-        if(numberOfItems >= circularArrayQueue.length){
-            Integer[] clone = new Integer[circularArrayQueue.length * 2];
+        if(numberOfItems >= circularArrayQueue.length){ // checks if the array is full
+            Integer[] clone = new Integer[circularArrayQueue.length * 2]; // creates a new array that is double the size of the circularArrayQueue
+            //we create a loop from i to numberOfItems so that all the elements present in the circularArrayQueue gets copied
             for (int i = 0; i < numberOfItems; i++) {
-                clone[i]= circularArrayQueue[head];
-                head++;
-                if(head == circularArrayQueue.length){
-                    head = 0;
-                }
+                clone[i]= circularArrayQueue[head]; // we start at head and not at i so that the oldest elements get copied first
+                head = (head+1)%circularArrayQueue.length; // we use modulo so that the head "wraps back around" when it reaches the end of the array
             }
-            circularArrayQueue = clone;
-            head=0;
+            circularArrayQueue = clone; //assigns the new array to the circularArrayQueue
+            head=0; //sets the head to 0
         }
-        tail = (head+numberOfItems)%circularArrayQueue.length;
-        circularArrayQueue[tail] = in;
-        numberOfItems++;
+        tail = (head+numberOfItems)%circularArrayQueue.length; // we use modulo so that when the tail reaches the end of the array it goes back to 0
+        circularArrayQueue[tail] = input; // assigns the input to the current position of the tail
+        numberOfItems++;// increments the nymber of items since an element has been added
 
 
 
@@ -41,17 +39,14 @@ public class CircularArrayQueue implements MyQueue {
     @Override
     public int dequeue() throws NoSuchElementException {
 
-        if(numberOfItems>0){
-            int temp = circularArrayQueue[head];
-            circularArrayQueue[head]=null;
-            head++;
-            if (head==circularArrayQueue.length){
-                head =0;
-            }
-            numberOfItems --;
-            return temp;
+        if(numberOfItems>0){ // checks if the array contains elements
+            int temp = circularArrayQueue[head]; //temporarily stores the values that is at the head
+            circularArrayQueue[head]=null; // sets the value that is at the head to null
+            head = (head +1)%circularArrayQueue.length; // increments the head (we use modulo so that the head reaches the end of the array it goes back to 0 )
+            numberOfItems --; // de-increments the number of items as a element has been removed from the array
+            return temp; //returns the temporarily stored value
         } else{
-            throw new NoSuchElementException("The Queue is empty");
+            throw new NoSuchElementException("The Queue is empty"); // if the array is empty then throw an exception.
         }
 
 
@@ -59,9 +54,9 @@ public class CircularArrayQueue implements MyQueue {
 
     @Override
     public boolean isEmpty() {
-        if (numberOfItems == 0){
-            return true;
-        }else{return false;}
+        if (numberOfItems == 0){ // checks if there are any items in the array
+            return true; // returns true if there aren't any
+        }else{return false;} //returns false if there are
     }
 
     @Override
@@ -73,8 +68,5 @@ public class CircularArrayQueue implements MyQueue {
         return circularArrayQueue.length - numberOfItems;
     }
 
-    public Integer[] getCircularArrayQueue() {
-        return circularArrayQueue;
-    }
 
 }
--- AlgTut2Ex2/src/CircularArrayRing.java
+++ AlgTut2Ex2/src/CircularArrayRing.java
@@ -7,28 +7,26 @@ public class CircularArrayRing<E> extends AbstractCollection<E> implements Ring<
     Object[] array;
     private int numberOfItems;
 
+    /*As it is impossible to create generic arrays in java I decided to use weak typing.
+     * I created an array of type object and then have a getter  for that
+      * array because I have to specify the type of object every time I want to
+      * access the value of a an element of that array.*/
+
     public CircularArrayRing(int size){
         array = new Object[size];
-    }
-    public CircularArrayRing(){array = new Object[10];}
+    } // creates a new array that is of the size specified as the argument
+    public CircularArrayRing(){array = new Object[10];} // default constructor creates a new array of size 10
 
-    E access(int i) {
+    E access(int i) { // return the value of the array that is stored in the i_th position
         @SuppressWarnings("unchecked")
         final E e = (E) array[i];
         return e;
     }
 
-    public void mutate(int i, E e) {
-        array[i] = e;
-    }
-
-    public Object[] getArray() {
-        return array;
-    }
-
     public boolean add(E e){
-            mutate(numberOfItems%array.length,e);
-            numberOfItems ++;
+            // we are using modulo when determining the index as we want the index to return to 0 when numberOfItems reaches the length of the array
+            array[numberOfItems%array.length]= e;
+            numberOfItems ++; //increments the number of items since we're adding an element
             return false;
     }
 
@@ -36,9 +34,13 @@ public class CircularArrayRing<E> extends AbstractCollection<E> implements Ring<
 
     @Override
     public E get(int index) throws IndexOutOfBoundsException {
+        //checks if the index passed as the argument can be returned
         if (index > (numberOfItems - 1) || index > array.length || index<0){
             throw new IndexOutOfBoundsException("Can't do that bro");
         } else {
+            /* since we want index 0 to return the previous elements,
+            we add -1 to the formula so that when index = 0 the access
+            method returns numberOfItems - 1. */
             return access((numberOfItems-1-index)%array.length);
 
         }
@@ -50,7 +52,14 @@ public class CircularArrayRing<E> extends AbstractCollection<E> implements Ring<
         Iterator<E> iterator = new Iterator<>() {
             int count = 0;
             @Override
+
             public boolean hasNext() {
+                /*this method checks if the array has another
+            that can be iterated on. We check if count is
+            greater or equal that the array length because
+            when count reaches the array length it means that
+            we have iterated through the whole array.
+            */
                 if(count >= array.length){
                     return false;
                 }else{
@@ -60,7 +69,12 @@ public class CircularArrayRing<E> extends AbstractCollection<E> implements Ring<
 
             @Override
             public E next() {
+                /*we are basically doing the same thing than with get(int index)
+                    but we increment count so that every time we call next() it returns
+                    the next previous element.
+                    * */
                 if (count < array.length) {
+
                     @SuppressWarnings("unchecked") final E e = (E) array[(numberOfItems - 1 - count) % array.length];
                     count++;
                     return e;
@@ -74,7 +88,7 @@ public class CircularArrayRing<E> extends AbstractCollection<E> implements Ring<
     }
 
     @Override
-    public int size() {
+    public int size() { //returns the size of the array
         if(numberOfItems>array.length){
             return array.length;
         }else{
@@ -82,14 +96,4 @@ public class CircularArrayRing<E> extends AbstractCollection<E> implements Ring<
         }
     }
 
-    public static void main(String[] args) {
-
-        CircularArrayRing<Integer> circularArrayRing = new CircularArrayRing<>(10);
-        for (int i=0; i <15; i++){
-            circularArrayRing.add(i);
-        }
-        for (int i = 0; i<circularArrayRing.getArray().length;i++) {
-            System.out.println(circularArrayRing.get(i));
-        }
-    }
 }
